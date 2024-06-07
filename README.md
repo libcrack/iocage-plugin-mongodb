a901e7bb71a64d5bb653ec472f7532936b91a865
libcrack <libcrack@users.noreply.github.com>
2024-06-07 23:48:24 +0200

    Implementing MongoDB iocage plugin

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ This service provides a mongo server and nothing more!
 
 ## Create the plugin
 
+Using a description local JSON file:
+
 ```sh
-iocage fetch --plugin-name "mongodb" --git_repository https://github.com/libcrack/iocage-plugin-index --branch mongodb --name mongodb vnet=On dhcp=On nat=Off
+iocage fetch --plugin-name mongodb.json --name mongodb vnet=On dhcp=On nat=Off
+```
+
+Through the Internet:
+
+```sh
+iocage fetch --plugin-name mongodb --git_repository https://github.com/libcrack/iocage-plugin-index --branch mongodb --name mongodb vnet=On dhcp=On nat=Off
 ```
 
 ### Clean-up after testing
@@ -14,36 +22,9 @@ iocage fetch --plugin-name "mongodb" --git_repository https://github.com/libcrac
 iocage destroy --recursive mongodb
 ```
 
-## Use the server from the host
-
 ### Login to a MongoDB shell from the host
 
 ```sh
-iocage exec mongodb bash -c 'PASS=$(</root/mysqlrootpassword); mysql -u root -p"${PASS}"'
+iocage exec mongodb bash -c 'whoami'
 ```
 
-### Execude SQL from the host shell
-
-```sh
-iocage exec mongodb bash -c 'PASS=$(</root/mysqlrootpassword); mysql -u root -p"${PASS}" -e "SHOW DATABASES"'
-```
-
-or
-
-```sh
-iocage exec mongodb bash -c 'PASS=$(\</root/mysqlrootpassword); mysql -u root -p"${PASS}"' << EOF
-SHOW DATABASES;
-USE MYSQL;
-SHOW TABLES;
-EOF
-```
-
-## Connect the server to an other jail
-
-```sh
-set jail_name=mongodb
-set socket=`zfs list -o mountpoint | grep "iocage/.*${jail_name}/root" | head -n 1`/var/run/mysql
-
-iocage exec second_jail mkdir /var/run/mysql
-iocage fstab -a second_jail $socket /var/run/mysql nullfs ro 0 0
-```
--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
 # Enable SSH
 sysrc sshd_enable="YES"
@@ -6,8 +6,10 @@ sysrc sshd_enable="YES"
 # Enable SSH root login
 sed -e 's|#PermitRootLogin no|PermitRootLogin yes|g' -i '' /etc/ssh/sshd_config
 
-echo "Start SSH server"
-service sshd start
+# Start SSH daemon
+if $(service sshd start 2>/dev/null >/dev/null) ; then
+    echo "Starting SSH server."
+fi
 
 # Configure remote SSH
 mkdir /root/.ssh
@@ -16,32 +18,17 @@ chmod 600 /root/.ssh/authorized_keys
 
 # Enable MongoDB
 sysrc mongod_enable="YES"
-sysrc mongod_pidfile=/var/db/mongodb/mongod.pid
+# sysrc mongod_pidfile=/var/db/mongodb/mongod.pid
+# sysrc mongod_pidfile=/var/db/mongodb/mongod.lock
 
 mkdir /var/log/mongod
-chown mongodb /var/log/mongod
+chown -R mongodb:mongodb /var/log/mongod
 
-CFG_FILE="/usr/local/etc/mongodb.conf"
+# Set MongoDB listening IP
+sed -e 's|127.0.0.1|0.0.0.0|g' -i '' /usr/local/etc/mongodb.conf
 
-mv ${CFG_FILE} ${CFG_FILE}.orig
-mv ${CFG_FILE}.template ${CFG_FILE}
-
-echo "Start mongoDB server"
-service mongod start
-
-# MYUSER="root"
-# (cat /dev/urandom | strings | tr -dc A-Za-z0-9\?\!\.\#\(\)\-\_ | head -c32; echo) > /root/mysqlrootpassword
-# PASS=$(</root/mysqlrootpassword)
-#
-# # set mysql-password
-# mysqladmin --user=$MYUSER password "$PASS"
-# echo "MySQL passwort set to $PASS"
-#
-# # Configure mysql
-# mysql -u $MYUSER -p"${PASS}" << EOF
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-# FLUSH PRIVILEGES;
-# DROP DATABASE IF EXISTS test;
-# EOF
+# Start MongoDB daemon
+if $(service mongod start 2>/dev/null >/dev/null) ; then
+    echo "Starting MongoBD server."
+fi
 
